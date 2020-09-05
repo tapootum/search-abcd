fd8b7d6874a97bc41623ea193b94262de3d73287
TaPooTum Channel <tapootumchannel@gmail.com>
2020-09-05 23:05:09 +0700

    add css

--- html/index.php
+++ html/index.php
@@ -1,1 +1,14 @@
-<?php phpinfo();?>
+<!DOCTYPE html>
+<head>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<html>
+    <div class="searchBox">
+        <input class="searchInput"type="text" name="" placeholder="Search">
+        <button class="searchButton" href="#">
+        <i class="material-icons">
+            search
+        </i>
+        </button>
+    </div>
+</html>
