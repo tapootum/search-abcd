22cff8edab9660439080162c257c7acb6c60408b
TaPooTum Channel <tapootumchannel@gmail.com>
2020-09-05 23:11:53 +0700

    change search

--- html/index.php
+++ html/index.php
@@ -3,12 +3,13 @@
   <link rel="stylesheet" href="styles.css">
 </head>
 <html>
-    <div class="searchBox">
-        <input class="searchInput"type="text" name="" placeholder="Search">
-        <button class="searchButton" href="#">
-        <i class="material-icons">
-            search
-        </i>
-        </button>
+<div class="flexbox">
+  <div class="search">
+    <h1>Search this site</h1>
+    <h3>Click on search icon, then type your keyword.</h3>
+    <div>
+      <input type="text" placeholder="Search . . ." required>
     </div>
+  </div>
+</div>
 </html>
